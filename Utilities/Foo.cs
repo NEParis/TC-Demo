d3ca6c274db3e2b401cedc8c90b1378bede4a154
Erik France <erik@neparis.com>
2011-12-19 23:06:27 -0600

    Committing demo files.

---
+++ Utilities/Foo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TC.Demo.Utilities
+{
+    internal class Foo
+    {
+        internal bool ValidateChoice(string choice)
+        {
+            if (choice.Length > 10)
+                return true;
+            else
+                return false;
+        }
+    }
+}
